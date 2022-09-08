d290d15f245be42e44c3dcdd614662ffc28189be
Kay Cee <se.kaycee@gmail.com>
2022-09-08 05:12:17 -0700

    modify 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char quote[];
-	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	fprintf(stderr, quote);
+	char *quote;
+	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", quote);
 	
 	return (1);
 }
