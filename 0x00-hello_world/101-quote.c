3b39e63314e814357cb17239886fda2ea3977e6c
Kay Cee <se.kaycee@gmail.com>
2022-09-08 05:14:51 -0700

    betty wahala for 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	char *quote;
 	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fprintf(stderr, "%s\n", quote);
-	
+
+	fprintf(stderr, "%s\n", quote);	
 	return (1);
 }
 
