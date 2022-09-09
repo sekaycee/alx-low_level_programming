5346f9fa20bfca5dd075413f952c30f85861366c
Kelechi Egbuta <se.kaycee@gmail.com>
2022-09-09 10:55:24 +0100

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
