1a3d57593c9aea2ac240b13a99ae70561ee80d16
Kelechi Egbuta <se.kaycee@gmail.com>
2022-09-09 11:50:25 +0100

    add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
