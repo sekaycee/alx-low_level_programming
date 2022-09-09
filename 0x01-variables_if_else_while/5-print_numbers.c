90384fa1bd9af1ce6f88a78c50892a33ab422b34
Kelechi Egbuta <se.kaycee@gmail.com>
2022-09-09 10:21:56 +0100

    add 5-print_numbers and correct 6-print_numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry Point
  *
  * Return: always 0 (Success)
  */
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
-		putchar(i);
-	putchar('\n');
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
 	return (0);
 }
 
