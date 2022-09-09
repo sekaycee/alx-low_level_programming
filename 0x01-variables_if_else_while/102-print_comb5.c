cb04aed73d57a47633bd97ae45ce1ea08c359a35
Kelechi Egbuta <se.kaycee@gmail.com>
2022-09-09 23:19:35 +0100

    correct typo 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int i, j, k, l, colj, coll;
+	int i, j, k, l, oj, ol;
 
 	i = j = k = l = 48;
 	while (i < 58)
@@ -21,9 +21,9 @@ int main(void)
 				l = 48;
 				while (l < 58)
 				{
-					colj = (10 * i) + j;
-					coll = (10 * k) + l;
-					if (coj < coll)
+					oj = (10 * i) + j;
+					ol = (10 * k) + l;
+					if (oj < ol)
 					{
 						putchar(i);
 						putchar(j);
