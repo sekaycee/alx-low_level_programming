158cc83349a57682541fdd5e8c1773b4b84185b4
Kelechi Egbuta <se.kaycee@gmail.com>
2022-09-09 10:15:54 +0100

    add 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
+
