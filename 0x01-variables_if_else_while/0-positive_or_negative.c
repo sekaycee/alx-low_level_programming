a2252286e71e434dfb168d2f46dded4287635f7c
Kelechi Egbuta <se.kaycee@gmail.com>
2022-09-09 12:38:44 +0100

    amend 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		print("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
