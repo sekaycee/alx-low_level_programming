3ea82582cee13a4c9c75a857c65f879a2180e5bc
Kay Cee <se.kaycee@gmail.com>
2022-09-13 08:29:47 -0700

    correct 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,12 @@
  */
 int print_sign(int n)
 {
-	if (n > 48)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 48 || n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
