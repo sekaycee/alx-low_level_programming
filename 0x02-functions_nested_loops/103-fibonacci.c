e2ed0cdc7fa321cecde5712f23f4a0cdb97c2426
Kay Cee <se.kaycee@gmail.com>
2022-09-13 13:53:41 -0700

    add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n1, n2, fn, fe;
+
+	n1 = 1;
+	n2 = 2;
+	fn = fe = 0;
+	for (; fn <= 4000000; fn = n1 + n2)
+	{
+		n1 = n2;
+		n2 = fn;
+		if ((n1 % 2) == 0)
+			fe += n1;
+	}
+	printf("%ld\n", fe);
+	return (0);
+}
+
