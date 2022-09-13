d9dbdcb03364798ed4b6967916c06134c0fe8960
Kay Cee <se.kaycee@gmail.com>
2022-09-13 07:50:46 -0700

    add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: integer argument
+ *
+ * Return: 1 if +ve 0 if 0 and -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
