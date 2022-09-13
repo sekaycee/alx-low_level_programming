1e5f614563d86703f0a3fd3f84fcebc4838f3813
Kay Cee <se.kaycee@gmail.com>
2022-09-13 09:30:34 -0700

    add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar('0' + i);
+			}
+			else
+			{
+				_putchar('0' + i / 10);
+				_putchar('0' + i % 10);
+			}
+			_putchar(':');
+			if (j < 10)
+			{
+				_putchar('0');
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar('0' + j / 10);
+				_putchar('0' + j % 10);
+			}
+			_putchar('\n');
+		}
+	}
+}
+
