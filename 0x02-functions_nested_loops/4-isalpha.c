320a0f08dc59f7ead6ae99455167618cdbc80c6f
Kay Cee <se.kaycee@gmail.com>
2022-09-13 04:49:50 -0700

    add 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+/**
+ * _isalpha - check if argument is an alphabet
+ * @c: integer argument
+ *
+ * Return: 1 if c is an alphabet 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) && (c > 96 && c < 123))
+		return (1);
+	else
+		return (0);
+}
+
