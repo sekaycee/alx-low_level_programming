27d3ae2c5bfd47b4af964b5dceebd476982b3cbd
Kay Cee <se.kaycee@gmail.com>
2022-09-13 11:59:50 -0700

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - add two integers
+ * @a: first integer argument
+ * @b: second integer argument
+ *
+ * Return: Sum of integers a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
