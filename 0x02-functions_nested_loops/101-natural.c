b4979015cbe28ef3e6a4156a507279ea1d58a549
Kay Cee <se.kaycee@gmail.com>
2022-09-13 13:31:54 -0700

    add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	i = 1;
+	while (i < 1024)
+	{
+		if((i % 3) == 0 || (i % 5) == 0)
+			i += j;
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
+
