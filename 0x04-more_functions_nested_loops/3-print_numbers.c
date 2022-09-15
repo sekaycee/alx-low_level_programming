493f87d3aea10f079069b343ef267b7d24cb7a1f
Kay Cee <se.kaycee@gmail.com>
2022-09-15 10:23:39 -0700

    add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digits 0 to 9 followed
+ * by a new line
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		_putchar(n);
+	_putchar('\n');
+}
+
