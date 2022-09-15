cbac0fa98e5a479a833f1543f9c812e291bc701b
Kay Cee <se.kaycee@gmail.com>
2022-09-15 11:05:49 -0700

    add 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
+
