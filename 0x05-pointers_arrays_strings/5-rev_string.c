5ab41ddadbe8badd6bdb55de436ace7a7cce5b95
Kay Cee <se.kaycee@gmail.com>
2022-09-19 07:29:29 -0700

    add 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,6 +6,21 @@
  */
 void rev_string(char *s)
 {
+	int c, i, j;
+	char temp;
 
+	c = 0;
+	while (s[c] != '\0')
+		c++;
+
+	for (i = 0; i < c - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			tmp = s[j];
+			s[j] = s[j - 1];
+			s[j - 1] = tmp;
+		}
+	}
 }
 
