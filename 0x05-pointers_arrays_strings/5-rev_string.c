d0e79dbf094c47fc1b5682a9ff096dbb4f3b95db
Kay Cee <se.kaycee@gmail.com>
2022-09-19 04:21:11 -0700

    add incomplete files

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: char pointer argument
+ */
+void rev_string(char *s)
+{
+
+}
+
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string. Begin with the first
+ * character followed by a new line
+ * @str: char pointer argument
+ */
+void puts2(char *str)
+{
+
+}
+
