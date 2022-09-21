c3ec26bde720a8ddebecd56a029b67fe626e95b8
Kay Cee <se.kaycee@gmail.com>
2022-09-21 08:45:30 -0700

    edit 3-strcmp.c

---
+++ 0x06-pointer_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string arg
+ * @s2: second string arg
+ * Return:
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, c;
+	
+	c = 0;
+	for (i = 0; c == 0; i++)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		c = s1[i] - s2[i];
+	}
+	return (c);
+}
+
