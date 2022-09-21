5c39a51f723bc400aac6ea47d036959aae7c5ff7
Kay Cee <se.kaycee@gmail.com>
2022-09-21 10:27:19 -0700

    modify main.h

--- 0x06-pointer_arrays_strings/main.h
+++ 0x06-pointer_arrays_strings/main.h
@@ -11,6 +11,7 @@ char *cap_string(char *s);
 char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
+int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
