dc40f087677fc2b85e43c08e998b07ab2f7d1a14
Kay Cee <se.kaycee@gmail.com>
2022-09-26 12:04:27 -0700

    correct 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a; int size)
 	int i, f_sum, s_sum;
 
 	f_sum = s_sum = 0;
-	for (i = 0; i < size * size; i++)
+	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
 			f_sum += a[i];
