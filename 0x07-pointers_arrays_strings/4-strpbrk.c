f4bcf59c0d6ae48ce84c38e5b7bca4c4d8bcfbe5
Kay Cee <se.kaycee@gmail.com>
2022-09-26 10:58:32 -0700

    edit 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
 /**
+ * _strpbrk - search a steing for any set of bytes
+ * @s: input string
+ * @accept: input substring
+ * Return: pointer to the byte in s.. matching first byte in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				return (s + i);
+	return ('\0');
+}
+
