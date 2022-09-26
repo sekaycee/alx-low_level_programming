27b562bc0bfbe95c65ee60ea8ed1fe221eee1514
Kay Cee <se.kaycee@gmail.com>
2022-09-26 06:07:47 -0700

    edit 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
 /**
+ * _strchr - locate a character in a string
+ * @s: input string
+ * @c: character to locate
+ * Return: pointer to char in string if found. NULL if not
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+		if (s[i] == c)
+			return (s[i]);
+
+	return (NULL);
+}
+
