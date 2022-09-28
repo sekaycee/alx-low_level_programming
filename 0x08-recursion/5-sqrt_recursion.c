0c033c8ddf1b85b01b74e2f649d6d75b0a8508b8
Kay Cee <se.kaycee@gmail.com>
2022-09-28 07:52:55 -0700

    edit 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * pow_op - return the natural square root of a number
+ * @n: given number
+ * @i: iterator
+ * Return: square root or -1
+ */
+int pow_op(int n, int i)
+{
+	if (i % (n / i) == 0)
+	{
+		if (i * (n / i) == n)
+			return (i);
+		else
+			return(-1);
+	}
+	return (0 + pow_op(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: given number
+ * Return: natural squre root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (pow_op(n, 2));
+}
+
