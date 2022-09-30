8e89bf7afcc669a3b75a91d174d0d4874a4a5bfd
Kay Cee <se.kaycee@gmail.com>
2022-09-30 07:52:18 -0700

    resolve unused arg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: argument vector
  * Return: always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
 
