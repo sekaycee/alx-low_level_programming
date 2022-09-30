276fbd693b2b9deb83cdb868a2d3a6a9b9187255
Kay Cee <se.kaycee@gmail.com>
2022-09-30 09:03:27 -0700

    edit 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
+	int i = 1, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (1);
+	}
+	while (i < argc)
+	{
+		if (!isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
 
