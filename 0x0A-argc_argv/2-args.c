163a6ff784aad4c6b57561581e10a2cf1f0198ec
Kay Cee <se.kaycee@gmail.com>
2022-09-30 08:03:03 -0700

    edit 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,10 +2,14 @@
 
 /**
  * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
 
