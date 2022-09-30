d28c27ec47c407dcd1b8bf5ff783a3f5dae44565
Kay Cee <se.kaycee@gmail.com>
2022-09-30 09:33:03 -0700

    add 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
+	int cents, nc = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		nc += 1;
+	}
+	printf("%d\n", nc);
 	return (0);
 }
 
