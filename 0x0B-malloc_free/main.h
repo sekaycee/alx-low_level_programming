f93d372c0b8f34b68c8e7aeff3c0f79908fd087f
Kay Cee <se.kaycee@gmail.com>
2022-10-03 03:56:24 -0700

    add main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char**);
+char **strtow(char *);
+
+#endif
+
