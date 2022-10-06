fa1d2652e7306db446cf1ba0c1aa285e03401ec8
Kay Cee <se.kaycee@gmail.com>
2022-10-06 10:12:45 -0700

    edit 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 
 /**
  * string_nconcat - concatenate two strings
- * @s1: firsr input string
+ * @s1: first input string
  * @s2: second input string
  * @n: input string limit
  * Return: pointer to new string
@@ -22,6 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		b++;
 	if (n >= b)
 		n = b;
+
 	s = malloc(sizeof(char) * (a + b + 1));
 	if (!s)
 	{
@@ -32,6 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i] = s1[i];
 	for (i = 0; i < n; i++)
 		s[a + i] = s2[i];
+
 	s[a + i] = '\0';
 	return (s);
 }
