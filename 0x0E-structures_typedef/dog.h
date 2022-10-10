31f3fb069d27de8ff6bd0c0f9f6e37760ae9e975
Kay Cee <se.kaycee@gmail.com>
2022-10-10 02:45:29 -0700

    edit dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog data structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
 
