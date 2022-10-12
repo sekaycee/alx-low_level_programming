e49b4e4cb4dcae8fdb75db68ca8d812661b6f069
Kay Cee <se.kaycee@gmail.com>
2022-10-12 08:19:35 -0700

    edit 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include "function_pointers.h"
+
+/**
+ * main - print opcodes
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{ 
+	int i, nb;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nb = atoi(argv[1]);
+
+	if (nb < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nb; i++)
+	{
+		printf("%02hhx", ((char *)main[i]));
+		/* printf("%02x", (char *)main[i] & 0xFF); */
+		if (i != nb - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
