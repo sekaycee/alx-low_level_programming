50aa6d3d5bcffbf64f6d372b5018693640b8e003
Kay Cee <se.kaycee@gmail.com>
2022-10-13 06:16:28 -0700

    add variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
+int _putchar(char);
+
+#endif
+
