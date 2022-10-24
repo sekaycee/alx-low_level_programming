63f690964531c4d0be22b6c33be78eb5ed15b1b1
Kay Cee <se.kaycee@gmail.com>
2022-10-24 02:43:22 -0700

    refactor code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	size_t i;
-	listint_t *new;
-	listint_t *in; /* insert node */
+	listint_t *new, *in; /* insert node */
 
 	in = *head;
 	if (idx != 0)
