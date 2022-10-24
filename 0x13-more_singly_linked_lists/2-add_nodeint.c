83bbe08396f4bacb51395f867f391c9b41bc1a3b
Kay Cee <se.kaycee@gmail.com>
2022-10-24 01:11:53 -0700

    refactor project code

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,12 +4,15 @@
  * add_nodeint - add a new node at the beginning
  * of a linked list
  * @head: head of a list
- * @n: n element
+ * @n: number to fill node
  * Return: address of the new element. NUll if failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
+
+	if (!head)
+		return (0);
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a node at the end of a list
+ * @head: current head address
+ * @n: number to fill the node
+ * Return: number of nodes
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *tmp = *head;
+
+	if (!head)
+		return (0);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+
+	return (new);
+}
+
