d4e7187739b5c8643f557f2e707aa3a94a06d34d
Kay Cee <se.kaycee@gmail.com>
2022-10-24 13:50:37 -0700

    add 103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find a loop and the beginning of loop
+ * @head: head of a list
+ * Return: pointer to the beginning of loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *a, *b;
+
+	if (!head || !head->next)
+		return (0);
+
+	a = b = head;
+	for (; a && b && b->next; a = a->next, b = b->next->next)
+		if (a == b)
+			break;
+
+	if (a != b)
+		return (0);
+
+	for (a = head; a != b; a = a->next)
+		b = b->next;
+	return (b);
+}
+
