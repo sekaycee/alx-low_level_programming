cf93a46a98b0028422d4a6e917b1293e74002681
Kay Cee <se.kaycee@gmail.com>
2022-10-24 01:38:57 -0700

    add 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a linked list
+ * @head: head of a list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	for (tmp = head; head; head = head->next)
+		free(tmp);
+}
+
