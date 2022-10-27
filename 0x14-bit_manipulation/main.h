d1609b868dd9f917f175258e36734c75f303ad7e
Kay Cee <se.kaycee@gmail.com>
2022-10-27 07:15:38 -0700

    add main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+unsigned int binary_to_uint(const char *);
+void print_binary(unsigned long int);
+int get_bit(unsigned long int, unsigned int);
+int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
+unsigned int flip_bits(unsigned long int, unsigned long int);
+int get_endianness(void);
+
+#endif /* MAIN_H */
+
