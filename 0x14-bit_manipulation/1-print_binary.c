f112e227c2046e414cad7c462bf2c3e945fc9134
Kay Cee <se.kaycee@gmail.com>
2022-10-27 08:01:20 -0700

    add 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary rep of a number
+ * @n: the unsigned long int
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
+
