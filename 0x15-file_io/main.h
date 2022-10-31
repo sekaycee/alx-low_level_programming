629c11e4c0c92bfdde41794bdfc8e1f83453e23d
Kay Cee <se.kaycee@gmail.com>
2022-10-31 09:07:00 -0700

    add main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
+
+#endif /* MAIN_H */
+
