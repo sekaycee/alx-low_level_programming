5eec4e44aa558db3b922f5d914b9c93df15e6497
Kay Cee <se.kaycee@gmail.com>
2023-01-05 21:40:57 +0100

    add 1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - generate a random number through
+ * 	a given string (hash number)
+ * @str: string
+ * Return: hash number
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
+}
