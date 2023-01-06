b9a85000c992b9923d154ea2d8f758d5010e8f7a
Kay Cee <se.kaycee@gmail.com>
2023-01-06 17:19:29 +0100

    debug 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,24 +6,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *table = ht;
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
+	if (ht == NULL)
+		return;
+
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		while ((tmp = node) != NULL)
 		{
-			node = ht->array[i];
-			while ((tmp = node) != NULL)
-			{
-				node = node->next;
-				free(tmp->key);
-				free(tmp->value);
-				free(tmp;
-			}
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp;
 		}
 	}
-	free(table->array);
-	free(table);
+	free(ht->array);
+	free(ht);
 }
