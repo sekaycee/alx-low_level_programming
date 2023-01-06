5602b3e9c80d5d57760c1de418c2c563b6d1796e
Kay Cee <se.kaycee@gmail.com>
2023-01-06 17:32:50 +0100

    debug code again

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -11,7 +11,7 @@ void shash_table_delete(shash_table_t *ht);
  * shash_table_create - create a sorted hash table
  * @size: the size of new sorted hash table
  * Return: if an error occurs - NULL
- * 	otherwise - a pointer to the new sorted hash table
+ * otherwise - a pointer to the new sorted hash table
  */
 shash_table_t *shash_table_create(unsigned long int size)
 {
@@ -119,11 +119,11 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 
 /**
  * shash_table_get - retrieve the value associated with
- * 	a key in a sorted hash table
+ * a key in a sorted hash table
  * @ht: pointer to the sorted hash table
  * @key: the key to get the value of
  * Return: if the key cannot be matched - NULL
- * 	otherwise - the value associated with key in ht
+ * otherwise - the value associated with key in ht
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
@@ -198,7 +198,6 @@ void shash_table_print_rev(const shash_table_t *ht)
  */
 void shash_table_delete(shash_table_t *ht)
 {
-	shash_table_t *head = ht;
 	shash_node_t *node, *tmp;
 
 	if (ht == NULL)
@@ -214,6 +213,6 @@ void shash_table_delete(shash_table_t *ht)
 		node = tmp;
 	}
 
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
