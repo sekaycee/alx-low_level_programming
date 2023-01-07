dc5da8bae5cd49a5ec35d134e6b5cc263dcb11b4
Kay Cee <se.kaycee@gmail.com>
2023-01-07 02:55:32 +0100

    debug 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -15,12 +15,13 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-		while ((node = tmp) != NULL)
+		while (node != NULL)
 		{
 			tmp = node->next;
 			free(node->key);
 			free(node->value);
 			free(node);
+			node = tmp;
 		}
 	}
 	free(ht->array);
