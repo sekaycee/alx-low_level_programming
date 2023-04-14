41bb7bc597b4f47dfd45fb981cf3ef7e790d791d
Kay Cee <se.kaycee@gmail.com>
2023-04-14 01:36:52 +0100

    [Fix] don't recurse.. use while

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,8 @@
 #include "search_algos.h"
 
 /**
- * _recurse - Search recursively for a value in an array of integers
- *            using the Binary search algorithm
- *
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return: index of value
- */
-int _recurse(int *array, size_t size, int value)
-{
-	int mid = size / 2;
-	size_t i;
-
-	if (!array || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-	printf("\n");
-
-	if (mid && size % 2 == 0)
-		mid--;
-
-	if (value == array[mid])
-		return (mid);
-
-	if (value < array[mid])
-		return (_recurse(array, mid, value));
-
-	mid++;
-	return (_recurse(array + mid, size - mid, value) + mid);
-}
-
-/**
- * binary_search - Wrap _recurse and return the index of the value
+ * binary_search - Search for a value in an array of integers using
+ *                 the Binary search algorithm
  *
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
@@ -46,11 +12,25 @@ int _recurse(int *array, size_t size, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i, left = 0, right = size - 1;
 
-	i = _recurse(array, size, value);
-	if (i >= 0 && array[i] != value)
-		return (-1);
+        if (!array)
+                return (-1);
 
-	return (i);
+        while (left <= right)
+        {
+                printf("Searching in array: ");
+                for (i = left; i < right; i++)
+                        printf("%d, ", array[i]);
+                printf("%d\n", array[i]);
+
+                i = left + (right - left) / 2;
+                if (array[i] == value)
+                        return (i);
+                if (array[i] > value)
+                        right = i - 1;
+                else
+                        left = i + 1;
+        }
+        return (-1);
 }
