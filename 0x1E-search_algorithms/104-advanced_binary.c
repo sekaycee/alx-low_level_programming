54874ad8c1ec5b3c54735e864b146bf556a77e1a
Kay Cee <se.kaycee@gmail.com>
2023-04-14 02:09:04 +0100

    [Fix] debug 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - Search for a value in an array of integers using
- *                 the Binary search algorithm
+ * _rsearch - Search recursively for a value in an array of
+ *            integers using the Binary search algorithm
  *
  * @array: a pointer to the first element of the array to search in
  * @size: the number of elements in array
@@ -10,27 +10,51 @@
  * Return: index of value - if exists
  *         -1 - otherwise
  */
-int advanced_binary(int *array, size_t size, int value)
+int _rsearch(int *array, size_t size, int value)
 {
-	size_t i, left = 0, right = size - 1;
+	size_t i, mid = size / 2;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 
-	while (left <= right)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+	printf("Searching in array");
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	printf("\n");
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
+	if (mid && size % 2 == 0)
+		mid--;
+
+	if (value == array[mid])
+	{
+		if (mid > 0)
+			return (_rsearch(array, mid + 1, value));
+		return ((int)mid);
 	}
-	return (-1);
+
+	if (value < array[mid])
+		return (_rsearch(array, mid + 1, value));
+
+	mid++;
+	return (_rsearch(array + mid, size - mid, value) + mid);
+}
+
+/**
+ * advanced_binary - Wrap _rsearch and validate the index returned
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: index of value - if checks passed
+ *         -1 - otherwise
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	int i;
+
+	i = _rsearch(array, size, value);
+	if (i >= 0 && array[i] != value)
+		return (-1);
+
+	return (i);
 }
